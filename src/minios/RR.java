18051e2e00b5d26bbe9d283f4a7f4a9c828a066a
Masonwp123 <masonparker868@gmail.com>
2026-09-18 14:21:53 -0400

    Fix RR edge case

--- src/minios/RR.java
+++ src/minios/RR.java
@@ -18,14 +18,21 @@ public class RR implements SchedulingAlgo {
         processDurations.put(p, processDurations.getOrDefault(p, 0) + 1);
         System.out.println("Process " + p.pid + " tick #" + processDurations.get(p));
 
+        // Fix edge case where IO wait is not entered until next time
+        // Process is running
+        Instruction inst = p.getCurrentInstruction();
+        if (inst == null || inst.remainingTicks <= 0) return;
+
         // If enough cpu cycles have done to reach time quant, set the process to switch out and add to the back of the queue
         if (processDurations.get(p) >= TIME_QUANT) {
             processDurations.put(p, 0);
+
+            // Tell the kernel to remove the process from the CPU
             p.state = Process.State.SWITCH;
+
             System.out.println("Process " + p.pid + " swapped out");
 
-            // Swap process to last in queue
-            readyQueue.remove(p);
+            // Add process to last in queue
             readyQueue.addLast(p);
         }
     }
